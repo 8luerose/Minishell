54d1266843c036c1a879782fdf8af03fa32f13b6
8luerose <8luerose@naver.com>
2023-08-25 21:07:25 +0900

    [REFACTOR] 1차 파싱 수정중 30줄ㅠㅠ

--- tmp/tmp_token.c
+++ tmp/tmp_token.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tmp_token.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: taehkwon <taehkwon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: taehkwon <taehkwon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 10:26:05 by seojchoi          #+#    #+#             */
-/*   Updated: 2023/08/25 10:02:02 by taehkwon         ###   ########.fr       */
+/*   Updated: 2023/08/25 18:42:35 by taehkwon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -297,79 +297,252 @@ void	get_token(char *input, t_list *list)
 //     free(buf);
 // }
 
-static void	handle_buffer(char **buf, char c)
+
+// 2차 수수정  코드
+// static void	handle_buffer(char **buf, char c)
+// {
+// 	char	*tmp;
+
+// 	tmp = *buf;
+// 	*buf = ft_strjoin_c(tmp, c);
+// 	free(tmp);
+// }
+
+// static void	init_flag(t_flag *flag)
+// {
+// 	flag->quo = 0;
+// 	flag->env = 0;
+// 	flag->env_with_quo = 0;
+// }
+
+// static void	handle_meta_char(char *input, int *i, char **buf, t_list *list)
+// {
+// 	char	*tmp;
+// 	t_flag	flag;
+
+// 	init_flag(&flag);
+
+// 	if (input[*i + 1] && input[*i] == '<' && input[*i + 1] == '<')
+// 	{
+// 		tmp = ft_strndup(&input[*i], 2);
+// 		(*i)++;
+// 	}
+// 	else if (input[*i + 1] && input[*i] == '>' && input[*i + 1] == '>')
+// 	{
+// 		tmp = ft_strndup(&input[*i], 2);
+// 		(*i)++;
+// 	}
+// 	else
+// 		tmp = ft_strndup(&input[*i], 1);
+
+// 	*buf = ft_strjoin_c(tmp, input[*i]);
+// 	make_node(*buf, flag, list);
+// 	free(tmp);
+// 	free(*buf);
+// 	*buf = ft_strdup("");
+// }
+
+// void	get_token(char *input, t_list *list)
+// {
+// 	int		i;
+// 	char	*buf;
+// 	t_flag	flag;
+
+// 	i = 0;
+// 	init_flag(&flag);
+// 	buf = ft_strdup("");
+
+// 	while (input[i])
+// 	{
+// 		if (input[i] != ' ')
+// 		{
+// 			if (is_meta(input[i]) && flag.quo == 0)
+// 				handle_meta_char(input, &i, &buf, list);
+// 			else
+// 			{
+// 				handle_buffer(&buf, input[i]);
+// 				if (flag.quo == 0 && (input[i] == '\"' || input[i] == '\''))
+// 					flag.quo = (input[i] == '\"') ? 1 : 2;
+// 				else if (flag.quo != 0 && (input[i] == '\"' || input[i] == '\''))
+// 					flag.quo = 0;
+// 			}
+// 		}
+// 		i++;
+// 	}
+// 	if (flag.quo != 0)
+// 		exit(1); // or handle error
+// 	if (buf[0] != 0)
+// 		make_node(buf, flag, list);
+// 	free(buf);
+// }
+
+// void append_character_to_buffer(char c, char **buf) {
+//     char *tmp = *buf;
+//     *buf = ft_strjoin_c(tmp, c);
+//     free(tmp);
+// }
+
+// void process_normal_character(char c, t_flag *flag, char **buf) {
+//     if (flag->quo == 0) {
+//         if (c == '\"')
+//             flag->quo = DOUBLE_Q;
+//         else if (c == '\'')
+//             flag->quo = SINGLE_Q;
+//         else if (c == '$')
+//             flag->env = 1;
+//     } else {
+//         if (c == '\"' && flag->quo == DOUBLE_Q)
+//             flag->quo = 0;
+//         else if (c == '\'' && flag->quo == SINGLE_Q)
+//             flag->quo = 0;
+//     }
+//     if (flag->quo == DOUBLE_Q && c == '$')
+//         flag->env_with_quo = 1;
+//     char *tmp = *buf;
+//     *buf = ft_strjoin_c(tmp, c);
+//     free(tmp);
+// }
+
+// void process_meta_character(char c, t_flag flag, char **buf, t_list *list) {
+//     char *tmp = *buf;
+//     *buf = ft_strjoin_c(tmp, c);
+//     free(tmp);
+//     if (!flag.quo)
+//         exit(1);
+//     if ((*buf)[0] != 0) {
+//         make_node(*buf, flag, list);
+//         flag.env = 0;
+//         flag.env_with_quo = 0;
+//     }
+//     if (c != ' ') {
+//         if (input[i + 1] && ((c == '<' && input[i + 1] == '<') || (c == '>' && input[i + 1] == '>'))) {
+//             tmp = ft_strndup(&input[i], 2);
+//             i++;
+//         } else
+//             tmp = ft_strndup(&input[i], 1);
+//         *buf = ft_strjoin_c(tmp, c);
+//         free(tmp);
+//         make_node(*buf, flag, list);
+//     }
+//     free(*buf);
+//     *buf = ft_strdup("");
+// }
+
+// void get_token(char *input, t_list *list) {
+//     int i = 0;
+//     char *buf = ft_strdup("");
+//     t_flag flag = {0, 0, 0}; // Initialize flag fields
+
+//     while (input[i]) {
+//         if (!is_meta(input[i]))
+//             process_normal_character(input[i], &flag, &buf);
+//         else if (is_meta(input[i]) && flag.quo > 0)
+//             append_character_to_buffer(input[i], &buf);
+//         else {
+//             process_meta_character(input[i], flag, &buf, list);
+//         }
+//         i++;
+//     }
+//     if (flag.quo)
+//         exit(1);
+//     if (buf[0] != 0)
+//         make_node(buf, flag, list);
+//     free(buf);
+// }
+
+void	handle_quotes(char input, t_flag *flag)
 {
-	char	*tmp;
-
-	tmp = *buf;
-	*buf = ft_strjoin_c(tmp, c);
-	free(tmp);
-}
-
-static void	init_flag(t_flag *flag)
-{
-	flag->quo = 0;
-	flag->env = 0;
-	flag->env_with_quo = 0;
-}
-
-static void	handle_meta_char(char *input, int *i, char **buf, t_list *list)
-{
-	char	*tmp;
-	t_flag	flag;
-
-	init_flag(&flag);
-
-	if (input[*i + 1] && input[*i] == '<' && input[*i + 1] == '<')
+	if (flag->quo == 0)
 	{
-		tmp = ft_strndup(&input[*i], 2);
-		(*i)++;
-	}
-	else if (input[*i + 1] && input[*i] == '>' && input[*i + 1] == '>')
-	{
-		tmp = ft_strndup(&input[*i], 2);
-		(*i)++;
+		if (input == '\"')
+			flag->quo = DOUBLE_Q;
+		else if (input == '\'')
+			flag->quo = SINGLE_Q;
+		else if (input == '$')
+			flag->env = 1;
 	}
 	else
-		tmp = ft_strndup(&input[*i], 1);
-
-	*buf = ft_strjoin_c(tmp, input[*i]);
-	make_node(*buf, flag, list);
-	free(tmp);
-	free(*buf);
-	*buf = ft_strdup("");
+	{
+		if (input == '\"' && flag->quo == DOUBLE_Q)
+			flag->quo = 0;
+		else if (input == '\'' && flag->quo == SINGLE_Q)
+			flag->quo = 0;
+	}
+	if (flag->quo == DOUBLE_Q && input == '$')
+		flag->env_with_quo = 1;
 }
 
+char	*append_char_and_free(char *buf, char input)
+{
+	char	*tmp;
+
+	tmp = buf;
+	buf = ft_strjoin_c(tmp, input);
+	free(tmp);
+	return (buf);
+}
+
+void	check_make_node(char *buf, t_flag *flag, t_list *list)
+{
+	if (buf[0] != 0)
+	{
+		make_node(buf, *flag, list);
+		flag->env = 0;
+		flag->env_with_quo = 0;
+	}
+}
+
+void	handle_special_char(char *input, int *i, t_flag flag, t_list *list)
+{
+	char	*tmp;
+	char 	*buf;
+
+	if (input[*i] != ' ')
+	{
+		if (input[*i + 1] && ((input[*i] == '<' && input[*i + 1] == '<') 
+			|| (input[*i] == '>' && input[*i + 1] == '>')))
+		{
+			tmp = ft_strndup(&input[*i], 2);
+			(*i)++;
+		}
+		else
+			tmp = ft_strndup(&input[*i], 1);
+		buf = ft_strjoin_c(tmp, input[*i]);
+		free(tmp);
+		make_node(buf, flag, list);
+	}
+	free(buf);
+}
 void	get_token(char *input, t_list *list)
 {
 	int		i;
 	char	*buf;
 	t_flag	flag;
 
-	i = 0;
 	init_flag(&flag);
 	buf = ft_strdup("");
-
+	i = 0;
 	while (input[i])
 	{
-		if (input[i] != ' ')
+		if (!is_meta(input[i]))
 		{
-			if (is_meta(input[i]) && flag.quo == 0)
-				handle_meta_char(input, &i, &buf, list);
-			else
-			{
-				handle_buffer(&buf, input[i]);
-				if (flag.quo == 0 && (input[i] == '\"' || input[i] == '\''))
-					flag.quo = (input[i] == '\"') ? 1 : 2;
-				else if (flag.quo != 0 && (input[i] == '\"' || input[i] == '\''))
-					flag.quo = 0;
-			}
+			handle_quotes(input[i], &flag);
+			buf = append_char_and_free(buf, input[i]);
+		}
+		else if (is_meta(input[i]) && flag.quo > 0)
+			buf = append_char_and_free(buf, input[i]);
+		else
+		{
+			if (flag.quo)
+				exit(1);
+			check_make_node(buf, &flag, list);
+			handle_special_char(input, &i, flag, list);
+			buf = ft_strdup("");
 		}
 		i++;
 	}
-	if (flag.quo != 0)
-		exit(1); // or handle error
-	if (buf[0] != 0)
-		make_node(buf, flag, list);
+	if (flag.quo)
+		exit(1);
+	check_make_node(buf, &flag, list);
 	free(buf);
 }

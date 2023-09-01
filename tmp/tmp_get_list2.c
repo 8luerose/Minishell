397156b98366f9454437b3e2ee5a1f9984dedc27
8luerose <8luerose@naver.com>
2023-09-01 17:11:01 +0900

    [REFACTOR] get_list 25줄 이하로 수정함(아직 tmp_get_list2 에 존재)

--- tmp/tmp_get_list2.c
+++ tmp/tmp_get_list2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tmp_get_list2.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: taehkwon <taehkwon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: taehkwon <taehkwon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 12:21:20 by seojchoi          #+#    #+#             */
-/*   Updated: 2023/09/01 14:10:39 by taehkwon         ###   ########.fr       */
+/*   Updated: 2023/09/01 17:10:05 by taehkwon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,7 +101,8 @@
 t_node	*process_word(t_node *p, t_data *new_data)
 {
     append_cmd(new_data, p->content);
-    return (p->next);
+	p = p->next;
+    return (p);
 }
 
 // Helper function to process REDIR_* types.
@@ -115,7 +116,8 @@ t_node	*process_redir(t_node *p, t_data *new_data)
         free_pipeline(new_data);
         exit(1);
     }
-    return (p->next);
+	p = p->next;
+    return (p);
 }
 
 // Helper function to connect new_data to pipe_data.
@@ -131,7 +133,9 @@ void    connect_new_data(t_data **pipe_data, t_data *new_data)
     {
         tmp = (*pipe_data);
         while (tmp && tmp->next)
+		{
             tmp = tmp->next;
+		}
         tmp->next = new_data;
     }
 }
@@ -148,7 +152,8 @@ void	get_list(t_list *list, t_data **pipe_data)
     {
         if (p->type == WORD)
             p = process_word(p, new_data);
-        else if (p->type == REDIR_IN || p->type == REDIR_OUT || p->type == HEREDOC_IN || p->type == HEREDOC_OUT)
+        else if (p->type == REDIR_IN || p->type == REDIR_OUT	\ 
+			|| p->type == HEREDOC_IN || p->type == HEREDOC_OUT)
             p = process_redir(p, new_data);
         else if (p->type == PIPE)
         {
@@ -159,7 +164,6 @@ void	get_list(t_list *list, t_data **pipe_data)
         else
             p = p->next;
     }
-    
     if (new_data->cmd_line || new_data->redir)
         connect_new_data(pipe_data, new_data);
     else

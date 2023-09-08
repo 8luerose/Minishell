54d3ad0ced8ebe67181a29a8993d513664684319
8luerose <8luerose@naver.com>
2023-09-09 06:48:14 +0900

    [STYLE] main 명세 삭제

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: taehkwon <taehkwon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 14:51:22 by seojchoi          #+#    #+#             */
-/*   Updated: 2023/09/09 06:36:41 by taehkwon         ###   ########.fr       */
+/*   Updated: 2023/09/09 06:46:03 by taehkwon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,31 +59,3 @@ int	main(int ac, char **av, char **ev)
 	}
 	return (g_stat);
 }
-
-
-//명세서
-//시그널 보내면 부모일때는 무시하고 자식 프로세스일때 시그널 처리
-
-//부모일 때(pid ==0 전일때) signal(SIGINT,SIG_IGN),
-//if (pid == 0) 뜨는 순간 signal 처리를 해준다
-
-//main 내 함수 몇가지 minishell, main_utils.c로 이동 
-//waitpid 수정, signal 수정
-//too many file moved to other file and new file
-//execute, expand, ft_(exit,export,unset), init, pipe, sytax 라인 수정
-
-
-
-
-//서지네 컴파일러
-//맥 클러스터
-//gcc -L/Users/seojchoi/.brew/opt/readline/lib -lreadline -I/Users/seojchoi/.brew/opt/readline/include *.c libft.a -g -fsanitize=address
-//맥 개인 맥북
-//gcc -L/opt/homebrew/opt/readline/lib -l readline -I/opt/homebrew/opt/readline/include *.c libft.a -g -fsanitize=address
-
-//태현이네 컴파일러
-//맥 클러스터
-//gcc -L/Users/taehkwon/.brew/opt/readline/lib -lreadline -I/Users/taehkwon/.brew/opt/readline/include *.c libft.a -g -fsanitize=address
-//맥 개인 맥북
-//gcc -L/opt/homebrew/opt/readline/lib -l readline -I/opt/homebrew/opt/readline/include *.c libft.a -g -fsanitize=address
-
